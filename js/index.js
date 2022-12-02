397653a22416f69b53d71173a421b7edc9a162f2
yutabaka <118330981+yutabaka@users.noreply.github.com>
2022-12-02 19:59:56 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="ngankimdaily" &&  pwd=="22062022"){
+    if(userName=="12122003" &&  pwd=="31062000"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
